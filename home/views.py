9bad810c2269cc4b465f38562369d486b6bbe60a
Vichu Bhai <vishnuvarthan@Vishnus-MacBook-Air.local>
2019-07-25 18:44:18 +0530

    Added Navbar

--- home/views.py
+++ home/views.py
@@ -3,4 +3,12 @@ from paypal.standard.forms import PayPalPaymentsForm
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import reverse
+from django.views.generic import View
+from .models import Product
 
+
+class HomeView(View):
+    template_name='home/home.html'
+    def get(self,request):
+        product=Product.objects.all()
+        return render(request,self.template_name,{'products':product})
